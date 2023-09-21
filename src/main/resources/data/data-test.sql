a95069edddd444321e161ea8590a7ccff21cdc0b
iksadNorth <iksadNorth@gmail.com>
2023-09-21 12:55:49 +0900

    테스트 API 데이터 중 ID 칼럼 제거. # 수정 이유 - ID를 정하고 데이터를 넣으면 PK 충돌이 발생함.

--- src/main/resources/data/data-test.sql
+++ src/main/resources/data/data-test.sql
@@ -1,7 +1,7 @@
 -- password : 1234
-INSERT INTO `member` (id, email, nickname, password)
-VALUES ('1', 'rladydqls99@naver.com', 'nick', '$2a$10$sw8G4sVnrHzd2rpGQ1GeoulDn3TvSA2N0v6LGGxwc/nGgMxMHZ.QW')
-     ,('2', 'rlaydld@asd.com', 'rasd', '$2a$10$sw8G4sVnrHzd2rpGQ1GeoulDn3TvSA2N0v6LGGxwc/nGgMxMHZ.QW');
+INSERT INTO `member` (email, nickname, password)
+VALUES ('rladydqls99@naver.com', 'nick', '$2a$10$sw8G4sVnrHzd2rpGQ1GeoulDn3TvSA2N0v6LGGxwc/nGgMxMHZ.QW')
+     ,('rlaydld@asd.com', 'rasd', '$2a$10$sw8G4sVnrHzd2rpGQ1GeoulDn3TvSA2N0v6LGGxwc/nGgMxMHZ.QW');
 
 INSERT INTO `company` (id, company_name, location)
 VALUES ('1233', 'samsung', 'seoul')
@@ -10,8 +10,8 @@ VALUES ('1233', 'samsung', 'seoul')
      ,('1236', 'samsung', 'seoul')
      ,('1237', 'toss', 'daejeon');
 
-INSERT INTO `comment` (id, comment, detail_id, member_id)
-VALUES ('1', '복지 좋음', '1234', '1')
-     ,('3', '사장님 착함', '1234', '1')
-     ,('4', '야근 많음', '1235', '2')
-     ,('5', '밥 맛있음', '1235', '2');
+INSERT INTO `comment` (comment, detail_id, member_id)
+VALUES ('복지 좋음', '1234', '1')
+     ,('사장님 착함', '1234', '1')
+     ,('야근 많음', '1235', '2')
+     ,('밥 맛있음', '1235', '2');
